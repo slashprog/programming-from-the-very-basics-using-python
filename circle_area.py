4daac70201ae2d26296aa6d95335c02cc6f0a233
Chandrashekar Babu <chandra@slashprog.com>
2024-02-23 20:49:00 +0530

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,4 @@
+radius = float(input("Enter radius of the circle: "))
+print(radius, type(radius))
+area = 3.14 * radius * radius
+print("The area of the circle is", area)
---
+++ greet.py
@@ -0,0 +1,2 @@
+username = input("What is your name ? ")
+print("Hello", username, "Welcome to Python coding!")
